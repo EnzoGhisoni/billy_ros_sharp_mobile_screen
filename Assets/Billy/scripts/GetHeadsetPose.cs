ce26070a600793b0cf575e077d499b6897523505
Enzo Ghisoni <enzo.ghisoni@hotmail.fr>
2021-01-24 15:20:11 +0100

    Move the robot with head motion
    Publish a Twist which contain the motion speed of the VR Headset with adapted to use with the motion model of the turtlebot (define in speed on /cmd_vel) but a bit too fast

---
+++ Assets/Billy/scripts/GetHeadsetPose.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR;
+
+
+public class GetHeadsetPose : MonoBehaviour
+{
+    // Declaration of variables for the headset
+    public GameObject headset;
+    private GameObject cube;
+
+    // Declaration of variable which will be published
+    private Vector3 previousPosition = Vector3.zero;
+    private Quaternion previousRotation = Quaternion.identity;
+    void Start()
+    {
+        //Initialize the Cube
+        cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the pose of the headset
+
+        cube.transform.position = new Vector3(headset.transform.position.x, headset.transform.position.y, headset.transform.position.z);
+        cube.transform.rotation = new Quaternion(headset.transform.rotation.x, headset.transform.rotation.y, headset.transform.rotation.z, headset.transform.rotation.w);
+        print(cube.transform.rotation.eulerAngles);
+
+
+
+    }
+
+    
+}
